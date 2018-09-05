8339b242cdb2817a8249cfb8abf1d5184b3751b2
YuyouWu <yuyouwu5@gmail.com>
2018-09-04 23:31:09 -0700

    Add Stock

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
-var portfolio = ['AAPL', 'MSFT', 'FB'];
+var portfolio = [];
+var stockNextId = 1;
 
 app.get('/', function (req, res) {
 	res.send('StockChart API');
@@ -24,10 +25,23 @@ app.post('/addStock', function (req, res) {
 	}
 
 	body.ticker = body.ticker.trim();
-	portfolio.push(body.ticker);
+  body.id = stockNextId++;
+
+	portfolio.push(body);
 	res.json(portfolio);
 });
 
+app.delete('/stock/:id', function (req, res) {
+  var stockId = parseInt(req.params.id, 10);
+	var matchedStock = _.findWhere(portfolio, {id: stockId});
+  console.log(matchedStock);
+  if (!matchedStock) {
+    res.status(404).json({"error": "no Stock found with that id"});
+  } else {
+    portfolio = _.without(portfolio, matchedStock);
+    res.json(matchedStock);
+  }
+});
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
