d822c31395e208d54ef218161562be54f3995157
YuyouWu <yuyouwu5@gmail.com>
2018-09-04 20:37:12 -0700

    Project init

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+var portfolio = [];
+
+app.get('/', function (req, res) {
+	res.send('StockChart API');
+});
+
+app.get('/portfolio', function (req, res) {
+	res.send('List of stock in a portfolio');
+});
+
+app.post('/addStock', function (req, res) {
+	var body = _.pick(req.body, 'ticker');
+
+	if (!_.isString(body.ticker) || body.ticker.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+	body.id = todoNextId++;
+
+	todos.push(body);
+
+	res.json(body);
+});
+
+
+app.listen(PORT, function () {
+	console.log('Express listening on port ' + PORT + '!');
+});
