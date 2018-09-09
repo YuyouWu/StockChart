551b5d7f83c016a12bd4a8225c47158272016ca4
YuyouWu <yuyouwu5@gmail.com>
2018-09-09 14:42:58 -0700

    Add POST /users and token method

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const _ = require('lodash');
 
 var bodyParser = require('body-parser');
 
@@ -11,12 +12,12 @@ var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
 	res.send('StockChart API');
 });
 
 //Get a list of tickers
-app.get('/portfolio', function (req, res) {
+app.get('/portfolio', (req, res) =>{
 	Ticker.find().then((tickers) =>{
 		res.send({tickers});
 	}, (e) => {
@@ -25,7 +26,7 @@ app.get('/portfolio', function (req, res) {
 });
 
 //Add ticker to portfolio
-app.post('/addTicker', function (req, res) {
+app.post('/addTicker', (req, res) => {
 	var ticker = new Ticker({
     ticker: req.body.ticker
   });
@@ -37,6 +38,20 @@ app.post('/addTicker', function (req, res) {
 	});
 });
 
-app.listen(PORT, function () {
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.listen(PORT, () => {
 	console.log('Express listening on port ' + PORT + '!');
 });
