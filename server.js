b5308313d0af266395aa83af7a19205abd3f0f83
YuyouWu <yuyouwu5@gmail.com>
2018-09-09 15:39:55 -0700

    Added middleware for user authentication. Added Delete /portfolio

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-var express = require('express');
+const express = require('express');
 const _ = require('lodash');
 
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
+const {mongoose} = require('./db/mongoose');
+const {ObjectID} = require('mongodb');
+mongoose.set('useCreateIndex', true);
+
 var {Ticker} = require('./models/ticker');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -38,6 +42,24 @@ app.post('/addTicker', (req, res) => {
 	});
 });
 
+app.delete('/portfolio/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Ticker.findByIdAndRemove(id).then((ticker) => {
+    if (!ticker) {
+      return res.status(404).send();
+    }
+
+    res.send({ticker});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 // POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
@@ -50,6 +72,11 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   })
+});
+
+//Get /users/me
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(PORT, () => {
