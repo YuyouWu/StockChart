f8863ddd039a10b9c0a0939011b407ff0a433317
YuyouWu <yuyouwu5@gmail.com>
2018-09-10 16:48:18 -0700

    Added /POST users/login

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.delete('/portfolio/:id', (req, res) => {
   });
 });
 
-// POST /users
+// POST /users - Add new users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
@@ -73,6 +73,20 @@ app.post('/users', (req, res) => {
     res.status(400).send(e);
   })
 });
+
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 
 //Get /users/me
 app.get('/users/me', authenticate, (req, res) => {
