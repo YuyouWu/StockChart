6abb2b140a6ec7a8b6edac674cea6fb78821148b
YuyouWu <yuyouwu5@gmail.com>
2018-09-25 20:33:41 +0800

    Linked /users api to register.js

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import axios from 'axios';
 
 class Register extends React.Component {
   	constructor() {
@@ -7,15 +8,36 @@ class Register extends React.Component {
     	this.state = {
 	      	email: '',
 	      	password: '',
-	      	password2: '',
-	      	errors: {}
-    	};
+	      	password2: ''
+	    };
+
+    	this.handleAddUser = this.handleAddUser.bind(this);
+	}
+
+
+	handleAddUser = (e) => {
+		e.preventDefault();
+		if(e.target.elements.confirmPassword.value !== e.target.elements.userPassword.value){
+			console.log("Passwords do not match");
+			return;
+		}
+
+		const newUser = {
+	    	email: e.target.elements.email.value,
+	    	password: e.target.elements.userPassword.value
+	    };
+
+		axios.post('/users', newUser).then(function (res){
+			console.log(newUser);
+		}).catch(function(err){
+			console.log(err);
+		});
 	}
 
 	render() {
 		return(
 			<div className="container">
-      			<Form>
+      			<Form onSubmit={this.handleAddUser}>
       				<br />
         			<FormGroup>
           				<Label for="email">Email</Label>
@@ -23,11 +45,11 @@ class Register extends React.Component {
 			        </FormGroup>
         			<FormGroup>
         				<Label for="password">Password</Label>
-          				<Input type="password" name="password" id="password" placeholder="Password" />
+          				<Input type="password" name="userPassword" id="userPassword" placeholder="Password" />
         			</FormGroup>
         			<FormGroup>
         				<Label for="confirmPassword">Confirm Password</Label>
-          				<Input type="password" name="password" id="confirmPassword" placeholder="Confirm Password" />
+          				<Input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password" />
         			</FormGroup>
 	                <Button>Submit</Button>
 		        </Form>
