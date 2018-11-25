052baf0f4c8423672725cdd53c1516778d1b53b6
YuyouWu <yuyouwu5@gmail.com>
2018-11-25 01:05:53 -0800

    Trendline tool

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -20,11 +20,13 @@ import { fitWidth } from "react-stockcharts/lib/helper";
 import { last, toObject } from "react-stockcharts/lib/utils";
 
 //Interaction
-import { TrendLine, DrawingObjectSelector } from "react-stockcharts/lib/interactive";
+import { TrendLine, FibonacciRetracement, DrawingObjectSelector } from "react-stockcharts/lib/interactive";
 import {
 	saveInteractiveNodes,
 	getInteractiveNodes,
 } from "./interactiveutils";
+
+import { Button, ButtonGroup } from "@blueprintjs/core";
 
 const candlesAppearance = {
 	wickStroke: function wick(d) {
@@ -51,7 +53,7 @@ class CandleStickStockScaleChart extends React.Component {
 		this.onKeyPress = this.onKeyPress.bind(this);
 		this.onDrawCompleteChart1 = this.onDrawCompleteChart1.bind(this);
 		this.handleSelection = this.handleSelection.bind(this);
-
+		this.onFibComplete1 = this.onFibComplete1.bin
 		this.saveInteractiveNodes = saveInteractiveNodes.bind(this);
 		this.getInteractiveNodes = getInteractiveNodes.bind(this);
 
@@ -59,8 +61,13 @@ class CandleStickStockScaleChart extends React.Component {
 
 		this.state = {
 			enableTrendLine: false,
+			enableFib: false,
 			trends_1: [], //for storing drawn trend line
+			retracements_1: [] //for storing retracement line
 		};
+
+		//Custom chart control
+		this.handleTrendLine = this.handleTrendLine.bind(this);
 	}
 
 	componentDidMount() {
@@ -113,38 +120,52 @@ class CandleStickStockScaleChart extends React.Component {
 		});
 	}
 
+	onFibComplete1(retracements_1) {
+		this.setState({
+			retracements_1,
+			enableFib: false
+		});
+	}
+
 	onKeyPress(e) {
 		const keyCode = e.which;
 		console.log(keyCode);
 		switch (keyCode) {
-		case 46: { // DEL
+			case 46: { // DEL
 
-			const trends_1 = this.state.trends_1
-				.filter(each => !each.selected);
-			if(this.canvasNode){
+				const trends_1 = this.state.trends_1
+					.filter(each => !each.selected);
+				if(this.canvasNode){
+					this.canvasNode.cancelDrag();
+				}		
+				this.setState({
+					trends_1
+				});
+				break;
+			}
+			case 27: { // ESC
+				//this.node_1.terminate();
 				this.canvasNode.cancelDrag();
-			}		
-			this.setState({
-				trends_1
-			});
-			break;
+				this.setState({
+					enableTrendLine: false
+				});
+				break;
+			}
+			case 68:   // D - Draw Fib
+			case 69: { // E - Enable Fib
+				this.setState({
+					enableFib: true
+				});
+				break;
+			}
 		}
-		case 27: { // ESC
-			//this.node_1.terminate();
-			this.canvasNode.cancelDrag();
-			this.setState({
-				enableTrendLine: false
-			});
-			break;
-		}
-		case 68:   // D - Draw trendline
-		case 69: { // E - Enable trendline
-			this.setState({
-				enableTrendLine: true
-			});
-			break;
-		}
-		}
+	}
+
+	//Custom chart control
+	handleTrendLine() {
+		this.setState({
+			enableTrendLine: true
+		});
 	}
 
 	render() {
@@ -184,83 +205,94 @@ class CandleStickStockScaleChart extends React.Component {
 		} : {};
 
 		return (
-			<ChartCanvas ref={this.saveCanvasNode} 
-				height={window.innerHeight-200}
-				ratio={ratio}
-				width={width}
-				margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
-				type={type}
-				seriesName={`TICKER_${this.state.suffix}`}
-				data={data}
-				xScale={xScale}
-				xAccessor={xAccessor}
-				displayXAccessor={displayXAccessor}
-				xExtents={xExtents}
-			>
-
-				<Chart id={1} yExtents={d => [d.high, d.low]}>
-					<XAxis axisAt="bottom" orient="bottom" ticks={10} {...xGrid}/>
-					<YAxis axisAt="right" orient="right" ticks={10} {...yGrid}/>
-					<MouseCoordinateY
-						at="right"
-						orient="right"
-						displayFormat={format(".2f")}
-					/>
-					<CandlestickSeries {...candlesAppearance}/>
-					<OHLCTooltip forChart={1} origin={[0, 0]} />
-					<ZoomButtons
-						onReset={this.handleReset}
-					/>
-					<TrendLine
-						ref={this.saveInteractiveNodes("Trendline", 1)}
-						enabled={this.state.enableTrendLine}
-						type="RAY"
-						snap={false}
-						snapTo={d => [d.high, d.low]}
-						onStart={() => console.log("START")}
-						onComplete={this.onDrawCompleteChart1}
-						trends={this.state.trends_1}
-					/>
-				</Chart>
-				<Chart
-					id={2}
-					height={100}
-					yExtents={d => d.volume}
-					origin={(w, h) => [0, h - 100]}
+			<div>
+				<ButtonGroup>
+					<Button icon="minus" onClick={this.handleTrendLine}></Button>
+				</ButtonGroup>
+				<ChartCanvas ref={this.saveCanvasNode} 
+					height={window.innerHeight-200}
+					ratio={ratio}
+					width={width}
+					margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
+					type={type}
+					seriesName={`TICKER_${this.state.suffix}`}
+					data={data}
+					xScale={xScale}
+					xAccessor={xAccessor}
+					displayXAccessor={displayXAccessor}
+					xExtents={xExtents}
 				>
-					<YAxis
-						axisAt="left"
-						orient="left"
-						ticks={5}
-						tickFormat={format(".2s")}
-					/>
 
-					<MouseCoordinateX
-						at="bottom"
-						orient="bottom"
-						displayFormat={timeFormat("%Y-%m-%d")}
-					/>
-					<MouseCoordinateY
-						at="left"
-						orient="left"
-						displayFormat={format(".4s")}
-					/>
+					<Chart id={1} yExtents={d => [d.high, d.low]}>
+						<XAxis axisAt="bottom" orient="bottom" ticks={10} {...xGrid}/>
+						<YAxis axisAt="right" orient="right" ticks={10} {...yGrid}/>
+						<MouseCoordinateY
+							at="right"
+							orient="right"
+							displayFormat={format(".2f")}
+						/>
+						<CandlestickSeries {...candlesAppearance}/>
+						<OHLCTooltip forChart={1} origin={[0, 0]} />
+						<ZoomButtons
+							onReset={this.handleReset}
+						/>
+						<TrendLine
+							ref={this.saveInteractiveNodes("Trendline", 1)}
+							enabled={this.state.enableTrendLine}
+							type="RAY"
+							snap={false}
+							snapTo={d => [d.high, d.low]}
+							onStart={() => console.log("START")}
+							onComplete={this.onDrawCompleteChart1}
+							trends={this.state.trends_1}
+						/>
+						{/* <FibonacciRetracement
+							ref={this.saveInteractiveNodes("FibonacciRetracement", 1)}
+							enabled={this.state.enableFib}
+							retracements={this.state.retracements_1}
+							onComplete={this.onFibComplete1}
+						/> */}
+					</Chart>
+					<Chart
+						id={2}
+						height={100}
+						yExtents={d => d.volume}
+						origin={(w, h) => [0, h - 100]}
+					>
+						<YAxis
+							axisAt="left"
+							orient="left"
+							ticks={5}
+							tickFormat={format(".2s")}
+						/>
 
-					<BarSeries
-						yAccessor={d => d.volume}
-						fill={d => (d.close > d.open ? "#6BA583" : "#FF0000")}
+						<MouseCoordinateX
+							at="bottom"
+							orient="bottom"
+							displayFormat={timeFormat("%Y-%m-%d")}
+						/>
+						<MouseCoordinateY
+							at="left"
+							orient="left"
+							displayFormat={format(".4s")}
+						/>
+
+						<BarSeries
+							yAccessor={d => d.volume}
+							fill={d => (d.close > d.open ? "#6BA583" : "#FF0000")}
+						/>
+					</Chart>
+					<CrossHairCursor />
+					<DrawingObjectSelector
+						enabled={!this.state.enableTrendLine}
+						getInteractiveNodes={this.getInteractiveNodes}
+						drawingObjectMap={{
+							Trendline: "trends"
+						}}
+						onSelect={this.handleSelection}
 					/>
-				</Chart>
-				<CrossHairCursor />
-				<DrawingObjectSelector
-					enabled={!this.state.enableTrendLine}
-					getInteractiveNodes={this.getInteractiveNodes}
-					drawingObjectMap={{
-						Trendline: "trends"
-					}}
-					onSelect={this.handleSelection}
-				/>
-			</ChartCanvas>
+				</ChartCanvas>
+			</div>
 		);
 	}
 }
